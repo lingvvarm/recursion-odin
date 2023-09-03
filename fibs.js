a40a3769097159ed82ddb909921a276a2a362b7a
lingvvarm <lingvvarm@gmail.com>
2023-09-03 21:10:06 +0300

    Finish first assignment

--- fibs.js
+++ fibs.js
@@ -2,7 +2,7 @@
 
 function fibs(n) {
   if (n <= 0) return 'incorrect number';
-  if (n == 1) return [1];
+  if (n == 1) return [0];
   if (n == 2) return [0, 1];
   let res = [0, 1];
   for (let i = 2; i < n; i++) {
---
+++ fibsRec.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function fibsRec(n) {
+    if (n == 1) return [0];
+    if (n == 2) return [0, 1];
+    return [...fibsRec(n-1), fibsRec(n-1)[n - 2] + fibsRec(n-1)[n - 3]];
+}
+
