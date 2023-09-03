4638988ed6f715e361042bb6035c99be7b126189
lingvvarm <lingvvarm@gmail.com>
2023-09-03 15:53:41 +0300

    Modify fibs

--- fibs.js
+++ fibs.js
@@ -1,6 +1,9 @@
 "use strict";
 
 function fibs(n) {
+  if (n <= 0) return 'incorrect number';
+  if (n == 1) return [1];
+  if (n == 2) return [0, 1];
   let res = [0, 1];
   for (let i = 2; i < n; i++) {
     res.push(res[i - 1] + res[i - 2]);
@@ -8,4 +11,3 @@ function fibs(n) {
   return res;
 }
 
-console.log(fibs(10));
